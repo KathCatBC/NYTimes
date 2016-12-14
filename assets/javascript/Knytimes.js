d1847665bdc2e2e0bbaa82bf3c1a545ffc0e9921
KathCatBC <KathCatBC@users.noreply.github.com>
2016-12-14 15:01:20 -0500

    Update Knytimes.js

--- assets/javascript/Knytimes.js
+++ assets/javascript/Knytimes.js
@@ -1,18 +1,12 @@
 // These variables are meant to correspond with the search fields which we'll be setting-up in html.
-// Also, at this point the 'for-loop' is just outputting documents' headlines to the console,
+// Also, at this point the 'for-loop' is populating an array with objects
 // but once we have divs, IDs, etc., I think we'll be able to make use of this scaffolding to send
 // the output of our choice to the appropriate html elements on screen.
 //
 // The problem 'how to dictate quanity of records to display' has not been addressed, yet.
-//
-// The above comments are valid as of 5:30pm on 12/11. Please delete/re-write these comments as you see fit. :-)
+
 
 // this will make multiple api calls to get > 10 records
-// I still have to figure out how to merge the results into 1 nice neat object
-// or deal with an object of the result objects
-
-
-
 
 
 $(document).ready(function(){
@@ -20,24 +14,26 @@ $(document).ready(function(){
 	// Right now, just testing them manually.
 	var apiKey = "62edb78d7ae94fd7846ceea0c4a8fcad";
 	var term = "digital rights"; // The search-term; corresponds to parameter 'q'.
-	var recordCount = ""; // The quantity of records to display. NOTE: NYTimes api DOES NOT accept this as a parameter.
+	
 	var yearStart = "2012"; // the year to put into parameter 'begin-date'. 
 	var yearEnd = "2013"; // the year to put into parameter 'end-date'.
 	var params = {}; // we can build the complete url dynamically using this variable. See 'if statements' below.
 	var articlesNeeded = 15;  // this will need to change via user input
-	var pagesNeeded = 0;
-	var partialPage = 0;
+	var pagesNeeded = 0;  // this is actually the number of the highest page number we need.
+	var partialPage = 0;  // used to calc pages in case user need # that is not a multiple of 10
 	var requestedArticles = [];
 	var result = {};
-	var tempresult = {};
 
+	var recordCount = 0; // used for pushing article info into the requestedArticles array
 	var p = 0;  // this will increment for each page requested - each page = 10 articles	
 
 	pagesNeeded = Math.floor(articlesNeeded/10);   // this will calculate the number
 	partialPage = articlesNeeded%10;				// of pages we need to get
 	if (partialPage === 0) {						// if the user request 35 articles we get
-		pagesNeeded--								// 40
+		pagesNeeded--								// 40.
+
 	}
+
 
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; // the base url to which we can add parameters/
 
@@ -72,22 +68,22 @@ $(document).ready(function(){
 		
 			var j = 0;  // for looping thru the results
 
-		// This will loop thru all the articles retrieved and push parts into an array of objects we don't have to display all the articles retrieved but deal with that on the front end instead of the backend.  Right now it just captures headline and web_url.  This still needs more testing to make sure the array is populated correctly
+		// This will loop thru all the articles retrieved and push parts into an array of objects we don't have to display all the articles retrieved but deal with that on the front end instead of the backend.  Right now it just captures headline and web_url.  
 
-debugger
-			var newArticle = {}
-				for (j=0; j<(result.response.docs).length; j++) {
-					console.log(result.response.docs[j].headline.main);
-					newArticle.headline = result.response.docs[j].headline.main
-					newArticle.weburl = result.response.docs[j].web_url
-					requestedArticles.push(newArticle)
-					console.log(newArticle)
-				}	
-		
+
+			
+			var weburlArticle;   // these are not really needed but make the code easier to read
+			var headlineArticle;
+
+			for (j=0; j<(result.response.docs).length; j++) {
+				headlineArticle = result.response.docs[j].headline.main;				
+				weburlArticle = result.response.docs[j].web_url;
+				requestedArticles.push({id: recordCount, headline: headlineArticle, weburl:weburlArticle})
+				recordCount ++
+			}	
 				
-			p++;    // increment p - p is the page number starting a zero
-
-			if (p < pagesNeeded){      // if we still need more pages
+			p++;    // increment p - p is the page number to retrieve
+			if (p <= pagesNeeded){      // if we still need more pages
 				url += '?' + $.param(params)+'&page='+ p;   // call the function getting the next page.
 				console.log(url);
 				getNYTpages();
@@ -106,12 +102,8 @@ debugger
 
 
 }
-
-	console.log(requestedArticles)  // this is our array of objects we want to display
+		console.log("full array of requested articles and then some")
+		console.log(JSON.stringify(requestedArticles))  // this is our array of objects we want to display
 
 }); // document.ready
-
-
-
-
 
